71cfedcbddedd3587406cb512165f55fd656f393
Doğan Derya <doganderya59@gmail.com>
2020-10-19 21:27:34 +0300

    Delete cert.sh

--- cert.sh
+++
@@ -1,3 +0,0 @@
- #!/bin/sh
-
- docker run --rm -it -v /container/nginx/html/knightby.com:/certs -v /container/certbot:/etc/letsencrypt certbot/certbot certonly --webroot -w /certs -d uif.knightby.com
